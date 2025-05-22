3596fb6423b963ff15e0194da291d7d2f02c1144
susanakasato <susana.nakasato@gmail.com>
2025-05-22 17:55:48 -0300

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { PatternModule } from "./module/pattern.module.js";
 async function bootstrap() {
   const app = await NestFactory.create(PatternModule);
   app.enableCors({
-    origin: "http://mystitchr.com"
+    origin: "https://mystitchr.com"
   })
   await app.listen(3000);
 }
